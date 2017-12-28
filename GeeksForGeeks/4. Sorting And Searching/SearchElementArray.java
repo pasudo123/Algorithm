e686658e1d6273a2df4cf1ffe31588279104d3ba
pasudo123 <pasudo222@gmail.com>
2017-12-28 15:21:45 +0900

    Update SearchElementArray Code : Linear BinarySearch & Bitonic BinarySearch & AscBS, DesBS

--- GeeksForGeeks/4. Sorting And Searching/SearchElementArray.java
+++ GeeksForGeeks/4. Sorting And Searching/SearchElementArray.java
@@ -33,36 +33,36 @@ public class SearchElementArray {
 			
 			int res = 0;
 			
-			// 일반적인 풀이 (문제 없음)
-			if((res = PASUDO_Solution(array, findNumber)) == -1){
-				System.out.println("OOPS! NOT FOUND");
-			}
-			else{
-				System.out.println(res);
-			}
 			
-			// GeeksforGeeks 풀이
-			if((res = GFG_pivotedBinarySearch(array, findNumber)) == -1){
-				System.out.println("OOPS! NOT FOUND");
-			}
-			else{
-				// 
-				if(res < 0){
-					int pivot = GFG_findPivot(array, 0, array.length - 1);
-					System.out.println(pivot + ", " + array[pivot]);
-					
-					
-				}
-				
-				System.out.println(">" + res);
-			}
+			// 일반적인 풀이 (문제 없음) : Linear 하게 찾는 방식이다.
+//			if((res = PASUDO_Solution(array, findNumber)) == -1){
+//				System.out.println("OOPS! NOT FOUND");
+//			}
+//			else{
+//				System.out.println(res);
+//			}
+			// *************************************************************
 			
+			
+			// 전혀 다른 풀이 방식이다.
+			// GeeksforGeeks 풀이 : Execution Time:0.1
+//			if((res = SearchElementArrayBinarySearch.searchBitonic(array, findNumber)) == -1){
+//				System.out.println("OOPS! NOT FOUND");
+//			}
+//			else{
+//				System.out.println(res);
+//			}
+			// *************************************************************
+			
+			
+			// GeeksforGeeks 풀이 : 
 //			if((res = GFG_ImproveSolution(array, 0, array.length-1, findNumber)) == -1){
 //				System.out.println("OOPS! NOT FOUND");
 //			}
 //			else{
 //				System.out.println(res);
 //			}
+			// *************************************************************
 		}
 	}
 	
@@ -78,39 +78,16 @@ public class SearchElementArray {
 		
 		return answer;
 	}
+		
 	
-	// Iterative : 반복 (Descending Order : 내림차순)
-	public static int PASUDO_DesIterativeBinarySearch(int[]array, int number){
-		int left = 0;
-		int right = array.length - 1;
-		int middle = 0;
-		
-		// O(logn)
-		while(left <= right){
-			middle = (left + right) / 2;
-			
-			if(array[middle] == number){
-				return middle;
-			}
-			
-			// 중간 값보다 더 우측에 존재한다. 
-			// 내림차순이니깐 중간 값보다 값이 더 작다.
-			if(array[middle] > number)
-				left = middle + 1;
-			
-			// 중간 값보다 더 좌측에 존재한다.
-			// 내림차순이니깐 중간 값보다 값이 더 크다.
-			//(array[middle] < number)
-			else
-				right = middle - 1;
-		}
-		
-		return -1;
-	}
-		
+	// Bitonic 시퀀스에는 맞지 않다. 
+	// 선형적으로 증가하는 배열에 대해서 rotate 가 일어난 경우만 해당되는 메소드
 	public static int GFG_pivotedBinarySearch(int[]array, int findNumber){
 		// 피봇을 찾는다.
 		int pivot = GFG_findPivot(array, 0, array.length-1);
+		
+		System.out.println("pivot : " + pivot);
+		System.out.println("value about pivot : " + array[pivot]);
 		
 		// 피봇을 찾지 못했다는 의미
 		// 배열은 올바르게 정렬되어있음을 말함.
@@ -118,18 +95,13 @@ public class SearchElementArray {
 			return Arrays.binarySearch(array, findNumber);
 		}
 		
-		if(pivot < 0){
-			
-		}
-		
 		if(array[pivot] == findNumber)
 			return pivot;
 		if(array[0] <= findNumber)
-			return Arrays.binarySearch(array, 0, pivot-1, findNumber);
+			return SearchElementArrayBinarySearch.PASUDO_AscIterativeBinarySearch(array, 0, pivot-1, findNumber);
 		
-		return Arrays.binarySearch(array, pivot+1, array.length-1, findNumber);
+		return SearchElementArrayBinarySearch.PASUDO_DesIterativeBinarySearch(array, pivot+1, array.length-1, findNumber);
 	}
-	
 	
 	/*
 	 * 피벗을 찾고 두 개의 하위 배열로 배열을 나눈 다음 바이너리 서치를 하는 것이다.
@@ -185,7 +157,7 @@ public class SearchElementArray {
 			return GFG_ImproveSolution(array, middle + 1, right, findNumber);
 		}
 		
-		// Not Sorted : array[left ... middle ] 정렬되어 있지 않은 상태
+		// Not Sorted : array[ left ... middle ] 정렬되어 있지 않은 상태
 		// 그러면, array[ (middle + 1) ... right ] 는 졍렬되어 있어야 한다.
 		// 
 		// WHY? 
